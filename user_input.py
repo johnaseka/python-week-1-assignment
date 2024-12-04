7cd4f5c0d8bdaa5f9c61794c94411398560b0766
johnaseka <johnaseka84@gmail.com>
2024-12-05 00:25:14 +0300

    dust

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Ask the user for their name
+name = input("your name: ")
+
+# Ask the user for their age and convert it to an integer
+age = input("Please enter your age: ")
+
+# Ask the user for their location
+location = input("Please enter your location: ")
+
+print("Hello ", name, "you are", age, " years old", " and live in", location)
